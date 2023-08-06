c61419dc66f78b44e69a3e79db0efae8586ee4a7
Jan Liphardt <jan.liphardt@stanford.edu>
2023-08-05 18:09:29 -0700

    Update query.py

--- bioe80/query.py
+++ bioe80/query.py
@@ -1,4 +1,4 @@
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index import StorageContext, load_index_from_storage
 import os
 
 if os.path.isfile("./storage/index_store.json") :
